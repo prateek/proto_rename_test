7ab237324b9582f4ae0adfbeb35a0d4e47e0acd9
Prateek Rungta <prungta2@gmail.com>
2018-09-28 20:59:24 -0400

    prove string == []bytes

--- main_test.go
+++ main_test.go
@@ -27,3 +27,41 @@ func TestRenameCompatibility(t *testing.T) {
 	require.Equal(t, old.FieldY, new.FieldYY)
 	require.Equal(t, old.FieldZ, new.FieldZ)
 }
+
+func TestBytesStringCompatiblility(t *testing.T) {
+	/*
+		This test generates a message of with String fields, encodes it and then decodes it
+		into a message which uses []byte instead.
+
+		message StringBackedMessage {
+			string fieldX  = 1;
+			string fieldYY = 2;
+			string fieldZ  = 3;
+		}
+
+		message BytesBackedMessage {
+			bytes fieldX  = 1;
+			bytes fieldYY = 2;
+			bytes fieldZ  = 3;
+		}
+	*/
+	stringMessage := newproto.StringBackedMessage{
+		FieldX:  string("abc"),
+		FieldYY: string("def"),
+		FieldZ:  string("some unicode chars 日本語"),
+	}
+
+	stringMessageBytes, err := proto.Marshal(&stringMessage)
+	require.NoError(t, err)
+
+	var bytesMessage newproto.BytesBackedMessage
+	require.NoError(t, proto.Unmarshal(stringMessageBytes, &bytesMessage))
+
+	require.Equal(t, stringMessage.FieldX, string(bytesMessage.FieldX))
+	require.Equal(t, stringMessage.FieldYY, string(bytesMessage.FieldYY))
+	require.Equal(t, stringMessage.FieldZ, string(bytesMessage.FieldZ))
+
+	require.Equal(t, []byte(stringMessage.FieldX), bytesMessage.FieldX)
+	require.Equal(t, []byte(stringMessage.FieldYY), bytesMessage.FieldYY)
+	require.Equal(t, []byte(stringMessage.FieldZ), bytesMessage.FieldZ)
+}
